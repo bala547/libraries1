6d795dcaa540e88f409c4d3209f70529a026a84b
bala547 <balakrishnaulli3@gmail.com>
2024-11-22 14:02:48 +0530

    Update runFlywayMigrations.groovy

--- runflywaymigrations/steps/runFlywayMigrations.groovy
+++ runflywaymigrations/steps/runFlywayMigrations.groovy
@@ -1,35 +1,30 @@
 void call() {
     node {
         // The Docker image for Flyway
-        def flywayImage = 'flyway/flyway:10.17.3'
-
-        // Ensure the ENVIRONMENT parameter is set before proceeding
-        if (!params.ENVIRONMENT) {
-            error "ENVIRONMENT parameter is not set. Please provide a valid environment."
-        }
+        def flywayImage = 'flyway/flyway:10.17.3' 
 
         // Print a message indicating that migrations will start
         echo "Running Flyway Migrations..."
 
-        // Print the value of ENVIRONMENT for debugging
-        echo "Loading environment configuration for: ${params.ENVIRONMENT}"
+        // Use the 'withCredentials' block to inject the stored secrets (SNOWFLAKE_URL, SNOWFLAKE_USERNAME, SNOWFLAKE_PASSWORD)
+        withCredentials([string(credentialsId: 'SNOWFLAKE_URL', variable: 'SNOWFLAKE_URL'),
+                         string(credentialsId: 'SNOWFLAKE_USERNAME', variable: 'SNOWFLAKE_USERNAME'),
+                         string(credentialsId: 'SNOWFLAKE_PASSWORD', variable: 'SNOWFLAKE_PASSWORD')]) {
 
-        // Load the environment-specific configuration based on ENVIRONMENT
-        def envConfig = load "environments/${params.ENVIRONMENT}.groovy"
-
-        // Run the Flyway migration command using the injected credentials
-        sh """
-            docker run --rm \\
-                -v \$(pwd)/migrations:/flyway/sql \\
-                -e JAVA_TOOL_OPTIONS=--add-opens=java.base/java.nio=ALL-UNNAMED \\
-                ${flywayImage} \\
-                -url='jdbc:snowflake://${envConfig.config.SNOWFLAKE_URL}' \\
-                -user='${envConfig.config.SNOWFLAKE_USER}' \\
-                -password='${envConfig.config.SNOWFLAKE_PASSWORD}' \\
-                -schemas='${envConfig.config.SNOWFLAKE_SCHEMA}' \\
-                -locations=filesystem:/flyway/sql \\
-                -baselineOnMigrate=true \\
-                migrate
-        """
+            // Run the Flyway migration command using the injected credentials
+            sh """
+                docker run --rm \\
+                    -v \$(pwd)/migrations:/flyway/sql \\
+                    -e JAVA_TOOL_OPTIONS=--add-opens=java.base/java.nio=ALL-UNNAMED \\
+                    ${flywayImage} \\
+                    -url='jdbc:snowflake://${SNOWFLAKE_URL}' \\
+                    -user='${SNOWFLAKE_USERNAME}' \\
+                    -password='${SNOWFLAKE_PASSWORD}' \\
+                    -schemas='BALA' \\
+                    -locations=filesystem:/flyway/sql \\
+                    -baselineOnMigrate=true \\
+                    migrate
+            """
+        }
     }
 }
