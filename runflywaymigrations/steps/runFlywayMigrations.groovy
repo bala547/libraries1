4316ed8cbf22ce13705fb5b16c76cb6a9cd3e1a5
bala547 <balakrishnaulli3@gmail.com>
2024-11-22 12:37:06 +0530

    Update runFlywayMigrations.groovy

--- runflywaymigrations/steps/runFlywayMigrations.groovy
+++ runflywaymigrations/steps/runFlywayMigrations.groovy
@@ -1,9 +1,17 @@
 void call() {
     node {
+        // The Docker image for Flyway
         def flywayImage = 'flyway/flyway:10.17.3' 
 
+        // Print a message indicating that migrations will start
         echo "Running Flyway Migrations..."
 
+        // Use the 'withCredentials' block to inject the stored secrets (SNOWFLAKE_URL, SNOWFLAKE_USERNAME, SNOWFLAKE_PASSWORD)
+        withCredentials([string(credentialsId: 'SNOWFLAKE_URL', variable: 'SNOWFLAKE_URL'),
+                         string(credentialsId: 'SNOWFLAKE_USERNAME', variable: 'SNOWFLAKE_USERNAME'),
+                         string(credentialsId: 'SNOWFLAKE_PASSWORD', variable: 'SNOWFLAKE_PASSWORD')]) {
+
+            // Run the Flyway migration command using the injected credentials
             sh """
                 docker run --rm \\
                     -v \$(pwd)/migrations:/flyway/sql \\
@@ -17,5 +25,6 @@ void call() {
                     -baselineOnMigrate=true \\
                     migrate
             """
+        }
     }
 }
