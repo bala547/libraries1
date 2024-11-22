0bd63d896887d9c1a50d5b3e3d107777d91975ac
bala547 <balakrishnaulli3@gmail.com>
2024-11-22 13:40:08 +0530

    Update runFlywayMigrations.groovy

--- runflywaymigrations/steps/runFlywayMigrations.groovy
+++ runflywaymigrations/steps/runFlywayMigrations.groovy
@@ -5,11 +5,7 @@ void call() {
 
         // Print a message indicating that migrations will start
         echo "Running Flyway Migrations..."
-
-        // Use the 'withCredentials' block to inject the stored secrets (SNOWFLAKE_URL, SNOWFLAKE_USERNAME, SNOWFLAKE_PASSWORD)
-        withCredentials([string(credentialsId: 'SNOWFLAKE_URL', variable: 'SNOWFLAKE_URL'),
-                         string(credentialsId: 'SNOWFLAKE_USERNAME', variable: 'SNOWFLAKE_USERNAME'),
-                         string(credentialsId: 'SNOWFLAKE_PASSWORD', variable: 'SNOWFLAKE_PASSWORD')]) {
+        def envConfig = load "environments/${params.ENVIRONMENT}.groovy"
 
             // Run the Flyway migration command using the injected credentials
             sh """
@@ -17,14 +13,13 @@ void call() {
                     -v \$(pwd)/migrations:/flyway/sql \\
                     -e JAVA_TOOL_OPTIONS=--add-opens=java.base/java.nio=ALL-UNNAMED \\
                     ${flywayImage} \\
-                    -url='jdbc:snowflake://${SNOWFLAKE_URL}' \\
-                    -user='${SNOWFLAKE_USERNAME}' \\
-                    -password='${SNOWFLAKE_PASSWORD}' \\
-                    -schemas='BALA' \\
+                    -url='jdbc:snowflake://${envConfig.config.SNOWFLAKE_URL}' \\
+                    -user='${envConfig.config.SNOWFLAKE_USER}' \\
+                    -password='${envConfig.config.SNOWFLAKE_PASSWORD}' \\
+                    -schemas='${envConfig.config.SNOWFLAKE_SCHEMA}' \\
                     -locations=filesystem:/flyway/sql \\
                     -baselineOnMigrate=true \\
                     migrate
             """
-        }
     }
 }
