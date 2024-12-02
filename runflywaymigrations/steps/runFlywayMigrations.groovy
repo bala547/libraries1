f767f11713b25b04e2c164b6f9a1e2a958d06939
bala547 <balakrishnaulli3@gmail.com>
2024-12-02 12:54:29 +0530

    Update runFlywayMigrations.groovy

--- runflywaymigrations/steps/runFlywayMigrations.groovy
+++ runflywaymigrations/steps/runFlywayMigrations.groovy
@@ -1,15 +1,5 @@
 void call() {
     node {
-        // Define the config object with environment variables
-        def config = [
-            ENV_NAME: 'dev', // Update this with the actual environment or parameter
-            SNOWFLAKE_SCHEMA: 'BALA'
-        ]
-
-        // Print debug information about the config object
-        echo "Config Object: ${config}"
-        echo "Running Flyway migrations for environment: ${config.ENV_NAME}"
-        echo "Using schema: ${config.SNOWFLAKE_SCHEMA}"
 
         // The Docker image for Flyway
         def flywayImage = 'flyway/flyway:10.17.3'
@@ -22,11 +12,8 @@ void call() {
                          string(credentialsId: 'SNOWFLAKE_USERNAME', variable: 'SNOWFLAKE_USERNAME'),
                          string(credentialsId: 'SNOWFLAKE_PASSWORD', variable: 'SNOWFLAKE_PASSWORD')]) {
 
-            // Set the ENV_NAME as an environment variable inside the 'sh' block
-            withEnv(["ENV_NAME=${config.ENV_NAME}"]) {
                 // Run the Flyway migration command using the injected credentials
                 sh """
-                    echo "Running Flyway migrations for environment: ${ENV_NAME}"
                     docker run --rm \\
                         -v \$(pwd)/migrations:/flyway/sql \\
                         -e JAVA_TOOL_OPTIONS=--add-opens=java.base/java.nio=ALL-UNNAMED \\
@@ -34,12 +21,11 @@ void call() {
                         -url='jdbc:snowflake://${SNOWFLAKE_URL}' \\
                         -user='${SNOWFLAKE_USERNAME}' \\
                         -password='${SNOWFLAKE_PASSWORD}' \\
-                        -schemas='${config.SNOWFLAKE_SCHEMA}' \\
+                        -schemas='BALA' \\
                         -locations=filesystem:/flyway/sql \\
                         -baselineOnMigrate=true \\
                         migrate
                 """
             }
-        }
     }
 }
