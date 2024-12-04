880cbb417bdaf16484535f39b9590efee236879e
bala547 <balakrishnaulli3@gmail.com>
2024-12-04 09:48:14 +0530

    Delete environments directory

--- environments/uat.groovy
+++
@@ -1,7 +0,0 @@
-config {
-    ENV_NAME = "uat"
-    SNOWFLAKE_URL = credentials('SNOWFLAKE_URL')
-    SNOWFLAKE_SCHEMA = "DEMO"
-    SNOWFLAKE_USER = credentials('SNOWFLAKE_USERNAME')
-    SNOWFLAKE_PASSWORD = credentials('SNOWFLAKE_PASSWORD')
-}
